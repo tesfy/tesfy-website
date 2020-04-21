c487671b4181dfd486214dac9242e3f86b604fe9
Andres Alvarez <a.alvarez.sor@gmail.com>
2020-04-21 23:09:43 +0200

    Move MDXProvider to layout

--- src/components/Docs/DocsLayout.tsx
+++ src/components/Docs/DocsLayout.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FC } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { MDXProvider } from '@mdx-js/react';
 import { Box, IconButton } from '@chakra-ui/core';
 import { FiMenu } from 'react-icons/fi';
 import Header from '../Header';
@@ -7,6 +8,7 @@ import Container from '../Container';
 import DocsSideNav from './DocsSideNav';
 import DocsSideNavContent, { NavContent, NavSection } from './DocsSideNavContent';
 import DocsDrawer from './DocsDrawer';
+import components from '../MDX';
 
 const query = graphql`
   query MyQuery {
@@ -107,7 +109,9 @@ const DocsLayout: FC = ({ children }) => {
       </DocsDrawer>
 
       <Box pt="62px" pl={[0, null, '18rem']}>
-        <Container maxWidth="46rem">{children}</Container>
+        <Container maxWidth="46rem">
+          <MDXProvider components={components}>{children}</MDXProvider>
+        </Container>
       </Box>
     </>
   );
