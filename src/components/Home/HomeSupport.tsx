da331e6b30173a66e57974bb116c26ddf54a9765
Pedro Valdivieso <pedro.valdivieso@gmail.com>
2020-11-07 21:37:19 +0000

    iOS and Android icons with URLs to the repos

--- src/components/Home/HomeSupport.tsx
+++ src/components/Home/HomeSupport.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Grid, Box } from '@chakra-ui/core';
-import { DiJsBadge, DiReact } from 'react-icons/di';
+import { DiJsBadge, DiReact, DiApple, DiAndroid } from 'react-icons/di';
 import Hightlight, { HightlightProps } from '../Hightlight';
 
 export type HomeSupportProps = Omit<HightlightProps, 'title' | 'image'>;
@@ -9,8 +9,18 @@ const HomeSupport: FC<HomeSupportProps> = props => {
   return (
     <Hightlight title="Supports" {...props}>
       <Grid gap={10} maxW="md" templateColumns={{ sm: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}>
-        <Box as={DiJsBadge} color="green.500" size="82px" />
-        <Box as={DiReact} color="green.500" size="82px" />
+        <a href="https://github.com/tesfy/tesfy">
+          <Box as={DiJsBadge} color="green.500" size="82px" />
+        </a>
+        <a href="https://github.com/tesfy/react-tesfy">
+          <Box as={DiReact} color="green.500" size="82px" />
+        </a>
+        <a href="https://github.com/tesfy/tesfy-swift">
+          <Box as={DiApple} color="green.500" size="82px" />
+        </a>
+        <a href="https://github.com/tesfy/tesfy-kotlin">
+          <Box as={DiAndroid} color="green.500" size="82px" />
+        </a>
       </Grid>
     </Hightlight>
   );
